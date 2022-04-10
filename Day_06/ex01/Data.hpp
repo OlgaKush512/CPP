bf33c230579cc38f0e6fd4fd207f32c0a24d63a8
Ольга Кушнирова <olgakusnirova@MacBook-Pro-Olga.local>
2022-04-10 19:24:12 +0200

    day06 ex01

---
+++ Day_06/ex01/Data.hpp
@@ -0,0 +1,33 @@
+#ifndef DATA_HPP
+# define DATA_HPP
+
+# include <iostream>
+# include <string>
+#include <cstdlib>
+#include <cfloat>
+
+struct Data
+{
+	int		myInt;
+	char	myChar;
+	float	myFloat;
+	double	myDouble;
+};
+
+uintptr_t serialize(Data* ptr)
+{
+	uintptr_t myPtr;
+
+	myPtr = reinterpret_cast<uintptr_t>(ptr);
+	return (myPtr);
+}
+
+Data* deserialize(uintptr_t raw)
+{
+	Data *myData;
+
+	myData = reinterpret_cast<Data *>(raw);
+	return (myData);
+}
+
+#endif
---
+++ Day_06/ex01/main.cpp
@@ -0,0 +1,41 @@
+#include "Data.hpp"
+
+int	main(void)
+{
+	Data myData = { 15, 'c', 0.42f, 0.42};
+	uintptr_t	myDataPtr;
+
+	myDataPtr = serialize(&myData);
+	std::cout << "The value of &myData by itself is "
+	<< std::showbase << std::hex << &myData << std::endl;
+
+	std::cout << "The value of &myData by myDataPtr is "
+	<< std::showbase << std::hex << myDataPtr << std::endl;
+
+	Data *myDataPtrNew = deserialize(myDataPtr);
+	std::cout << "The value of &myDataPtr by itself is "
+	<< std::showbase << std::hex << myDataPtr << std::endl;
+
+	std::cout << "The value of &myDataPtr by myDataPtrNew is "
+	<< std::showbase << std::hex << myDataPtrNew << std::endl;
+
+	std::cout << std::endl << "More ..." << std::endl << std::endl;
+
+	std::cout << "myData structure" << std::endl;
+	std::cout << "Int: " << myData.myInt << std::endl;
+	std::cout << "Char: " << myData.myChar << std::endl;
+	std::cout << "Float: " << myData.myFloat << std::endl;
+	std::cout << "Double: " << myData.myDouble << std::endl << std::endl;
+
+	std::cout << "myDataPtrNew structure" << std::endl;
+	std::cout << "Int: " << myDataPtrNew->myInt << std::endl;
+	std::cout << "Char: " << myDataPtrNew->myChar << std::endl;
+	std::cout << "Float: " << myDataPtrNew->myFloat << std::endl;
+	std::cout << "Double: " << myDataPtrNew->myDouble << std::endl;
+
+
+
+	// delete myZombiePtr;
+
+	return (0);
+}
