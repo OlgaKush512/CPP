a4aa586e895722dea0ab61c8beb0da97b565bc16
Olga KUSHNIROVA <okushnir@e3r7p23.clusters.42paris.fr>
2022-04-18 17:47:02 +0200

    Day02 pour valid

--- Day_02/ex00/Fixed.cpp
+++ Day_02/ex00/Fixed.cpp
@@ -14,34 +14,34 @@
 
 Fixed::Fixed(void) : _nbStock(0)
 {
-	std::cout << "Default constructer called. Adress :" << this << std::endl;
+	std::cout << "Default constructer called. Adress : " << this << std::endl;
 }
 
 Fixed::Fixed(Fixed const &other) : _nbStock(other.getRawBits())
 {
-	std::cout << "Copy constructor called, address :" << this << std::endl;
+	std::cout << "Copy constructor called, address : " << this << std::endl;
 	this->_nbStock = other.getRawBits();
 }
 
 Fixed::~Fixed(void)
 {
-	std::cout << "Destructeur called. Adress :" << this << std::endl;
+	std::cout << "Destructeur called. Adress : " << this << std::endl;
 }
 
 Fixed & Fixed::operator = (const Fixed &other)
 {
-	std::cout << "Copy assignment operator called, address :" << this << std::endl;	
+	std::cout << "Copy assignment operator called, address : " << this << std::endl;	
 	this->_nbStock = other.getRawBits();
 	return (*this);
 }
 
 int Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << this << std::endl;	
+	std::cout << "getRawBits member function called, address : " << this << std::endl;	
 	return (this->_nbStock);
 }
 void Fixed::setRawBits(int const raw)
 {
-	std::cout << "setRawBits member function called" << this << std::endl;	
+	std::cout << "setRawBits member function called, address : " << this << std::endl;	
 	this->_nbStock = raw;
 }
